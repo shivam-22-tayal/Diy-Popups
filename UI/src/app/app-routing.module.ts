7ac651f37e73a5a58d7dd6be9442be0010ae5776
Shivam Tayal <shitaya1@in.ibm.com>
2019-04-09 23:19:51 +0530

    Initial commit

---
+++ UI/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+const routes: Routes = [
+  {path: '', component: LandingpageComponent},
+  {path: 'landingpage', component: LandingpageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
