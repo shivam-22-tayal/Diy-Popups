cd6cbbf72341e48b66f71877f8f85a31a267ba95
vedavathigali <gvedava1@in.ibm.com>
2019-04-12 18:35:30 +0530

    ui page

---
+++ Service/src/main/java/com/ibm/diypopups/service/AdvertisementService.java
@@ -0,0 +1,13 @@
+package com.ibm.diypopups.service;
+
+import java.util.List;
+
+import com.ibm.diypopups.model.Advertisement;
+
+public interface AdvertisementService {
+
+	List<Advertisement> getAllAdvertisement();
+    Advertisement getAdvertisementById(int advtId);
+	boolean addAdvertisement(Advertisement advt);
+	
+}
