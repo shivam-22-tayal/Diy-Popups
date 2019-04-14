0b88ca7b5ce440443d3173cf638f71afa1372e79
Satyam Negi <satyasn1@in.ibm.com>
2019-04-14 17:04:44 +0530

    Integrated Owner-Dashboard

--- UI/src/app/owner-dashboard/owner-dashboard.component.ts
+++ UI/src/app/owner-dashboard/owner-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdvertisementService } from "../advertisement.service";
+import { Advertisement } from "../advertisement.model";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-owner-dashboard',
@@ -7,11 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OwnerDashboardComponent implements OnInit {
 
-  constructor() { }
+  advts: Advertisement[];
 
-  
+  category;
+  product;
+  dop;
+  desc;
+  constructor(private service: AdvertisementService,
+              private router : Router
+    ) { }
+
+
   ngOnInit() {
+    this.getAllAdvt();
+  }
 
+  getAllAdvt(){
+    this.service.getAllAdvt().subscribe(
+      data =>{
+        this.advts = data;
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+  }
+
+  submitAdvt(){
+
+    if(this.category!=undefined && this.product!=undefined && this.dop!=undefined && this.desc!=undefined)
+    {
+   let advtInfo ={
+     categoryadd: this.category,
+     product: this.product,
+     dop: this.dop,
+     description: this.desc
+   }
+
+      this.service.writeAdvt(advtInfo)
+      .subscribe(
+        response => {
+          alert("Advertisement has been posted...!!");
+          this.router.navigateByUrl("/showAdd");
+
+      },
+      error => {
+        console.log(error);
+
+      });
+
+  }
+    this.category="";
+    this.product="";
+    this.dop="";
+    this.desc="";
   }
 
 }
